1c892a72aeb40fb9da9d2f5e04e24d110ee4a1f3
zanama <57731386+zanmama@users.noreply.github.com>
2021-08-10 16:46:54 -0400

    new folders for setup

---
+++ index.js
@@ -0,0 +1,27 @@
+// DEPENDENCIES ===========
+
+// DATA
+
+const inquirer = require("inquirer")
+
+
+
+
+
+function writeToFile
+
+
+const selectTeamMemberQuestion = [
+
+]
+
+function caputreInternInfo() {
+    inquirer.prompt(internQuestions).then(function (internInfo));
+}
+
+
+
+function init() {
+    inquirer
+     .prompt
+}
---
+++ src/generateHTML.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+//create function to generate markdown for README
+function generateHTML(data) {
+    return ``;
+}
+
+//initialization ==========
+determineMemberIcon();
+
+//module.exports makes this file's function below for generateHTML
