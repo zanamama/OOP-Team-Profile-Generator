eb1cec6b314b3a4b2fdc3e52a22a71efeb702cd6
zanama <57731386+zanmama@users.noreply.github.com>
2021-08-10 20:14:18 -0400

    created questions for manager in selecting employees

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
-// DEPENDENCIES ===========
+// DEPENDENCIES & Packages ===========
+const inquirer = require("inquirer");
+const fs = require("fs")
+const employee = require("./lib/Employee");
+const intern = require("./lib/Intern");
+const engineer = require("./lib/Engineer");
+const manager = require("./lib/Manager");
+const genHTML = require("./src/generateHTML");
 
-// DATA
+// DATA ===========
+const filename = "index.html";
+let teamArray = [];
 
-const inquirer = require("inquirer")
+// Questions in Array for User Input
+const initialManagerQuestions = [
+    {
+        type: "input",
+        name: "managerName",
+        message: "What is your team Manager's name?",
+    },
+    {
+        type: "input",
+        name: "managerID",
+        message: "What is your Manager's ID?",
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "What is your team Manager's email address?",
+    },
+    {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What is your manager's office number?",
+    },
+];
 
-
+// Array of team members selected by Manager
+const selectTeamMemberQuestion = [
+    {
+        type: "list",
+        name: "How many years of experience do you have?"
+        input: "",
+        choices: ["Under 3 years", "3-5 Years", "5-10 Years" , "10+ Years"],
+    }
+]
 
 
 
