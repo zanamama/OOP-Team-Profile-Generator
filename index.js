56bb1200e78c4795c026df0b33f6bd16fc10dc27
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-19 10:41:55 -0400

    added intern questions to index.js. Fixed init

--- index.js
+++ index.js
@@ -14,64 +14,41 @@ let finalHtml = ``;
 
 // Questions in Array for User Input
 const Questions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is the employee's name?",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is his/her ID?",
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is the employee's email address?"
-    },
-    console.log(questions)
+  {
+    type: "input",
+    name: "name",
+    message: "What is the employee's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is his/her ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the employee's email address?",
+  },
+  console.log(questions),
 ];
 
 // Array of team members selected by Manager
-const managerQuestions = Questions.concat [
-    {
-        type: "list",
-        name: "officeNum",
-        message: "What's the Manager's office number?"
-    }
-);
+const managerQuestions = Questions.concat({
+  type: "list",
+  name: "officeNum",
+  message: "What's the Manager's office number?",
+});
 
- const engineerQuestions = Questions.concat [
-    {
-        type: "list",
-        name: "github",
-        message: "What is the GitHub username of this Engineer?",
-    }
-const engineerQuestions = Questions.concat [
-    {
-        type: "list",
-        name: "github",
-        message: "What is the GitHub username of this Engineer?",
-    }
-]
+const engineerQuestions = Questions.concat({
+  type: "list",
+  name: "github",
+  message: "What is the GitHub username of this Engineer?",
+});
 
+const internQuestions = Questions.concat({
+  type: "input",
+  name: "school",
+  message: "What college/university is the Intern currently attending?",
+});
 
-
-function writeToFile();
-
-
-const selectTeamMemberQuestion = [
-
-]
-
-function caputreInternInfo() {
-    inquirer.prompt(internQuestions).then(function (internInfo))
-    }
-
-
-
-
-function init() {
-    inquirer
-     .prompt
-}
+init();
